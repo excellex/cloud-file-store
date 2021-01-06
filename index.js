7aa1c9b557f67e24bb5e8d2908d093a31acffe5d
llixgrijb <excellex@ya.ru>
2021-01-06 18:42:12 +0300

    github upload

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const path = require('path')
 require('dotenv').config()
 
 const authRouter = require('./routes/route.auth')
+const indexRouter = require('./routes/route.index')
 // const app = express()
 app.use(morgan("dev"))
 app.use(express.json())
-app.use(express.static('/home/user/elbrus/projects/cloudstore/public'))
-app.use(express.static('/home/user/elbrus/projects/cloudstore/public/build'))
+// app.use(express.static('/home/user/elbrus/projects/cloudstore/public'))
+app.use(express.static(path.join(__dirname, 'public', 'build')))
+
+
+app.use('/', indexRouter)
 app.use('/api/auth', authRouter)
 
 const listen = async () => {
@@ -28,8 +32,6 @@ const listen = async () => {
 }
 // console.log('\n\n\n\n\n\n',app.locals,'\n\n\n\n\n\n');
 
-app.get('/', (req, res) => {
-  res.sendFile('/home/user/elbrus/projects/cloudstore/public/index.html')
-})
+
 
 listen()
